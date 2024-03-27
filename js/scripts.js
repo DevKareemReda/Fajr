d017c9395da5ef8be5ba8e579c4ab5af9b50cd60
kareem reda <147895396+DevKareemReda@users.noreply.github.com>
2024-03-27 04:48:20 -0700

    Created new Sections in home page

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(function () {
+    var slider= tns({
+        container: ".slider",
+        speed: 700,
+        items: 3,
+        loop: false,
+        controlsPosition: 'top',
+        nav: false,  
+        loop: true,
+        mouseDrag: true,
+        controlsContainer: '.slider-nav',
+        responsive: {
+          0: {
+            items: 1,
+            edgePadding: 20
+          },
+          700: {
+            items: 2,
+          },
+          900: {
+            items: 3,
+            edgePadding: 120,
+          }
+        }
+    })
+})
+
+
+let allLanguages = document.querySelectorAll(".language li a");
+
+allLanguages.forEach(el => {
+    el.onclick = function (e) {
+        e.preventDefault();
+        allLanguages.forEach( el => el.classList.remove("active"))
+        this.classList.add("active")
+    }
+})
