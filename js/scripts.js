f917a394fa4b8a0f54e98cb58cbba99a36fa1b46
kareem reda <147895396+DevKareemReda@users.noreply.github.com>
2024-03-28 23:33:35 -0700

    Created life in egypt and contact us and more

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,42 @@
 $(function () {
-    var slider= tns({
-        container: ".slider",
-        speed: 700,
-        items: 3,
-        loop: false,
-        controlsPosition: 'top',
-        nav: false,  
-        mouseDrag: true,
-        controlsContainer: '.slider-nav',
-        responsive: {
-          0: {
+  var slider = tns({
+    container: ".slider",
+    speed: 700,
+    items: 3,
+    loop: false,
+    controlsPosition: "top",
+    nav: false,
+    mouseDrag: true,
+    controlsContainer: ".slider-nav",
+    responsive: {
+      0: {
         items: 1,
-          },
-          700: {
-            items: 2,
-          },
-          900: {
-            items: 3,
-            edgePadding: 120,
-          }
-        }
-    })
-})
-
+      },
+      700: {
+        items: 2,
+      },
+      900: {
+        items: 3,
+        edgePadding: 120,
+      },
+    },
+  });
+});
 
 let allLanguages = document.querySelectorAll(".language li a");
+allLanguages.forEach((el) => {
+  el.onclick = function (e) {
+    e.preventDefault();
+    allLanguages.forEach((el) => el.classList.remove("active"));
+    this.classList.add("active");
+  };
+});
 
-allLanguages.forEach(el => {
-    el.onclick = function (e) {
-        e.preventDefault();
-        allLanguages.forEach( el => el.classList.remove("active"))
-        this.classList.add("active")
-    }
+let allTabs = document.querySelectorAll(".tabs li a");
+allTabs.forEach(el => {
+  el.onclick = function (e) {
+    e.preventDefault();
+    allTabs.forEach((el) => el.classList.remove("active"));
+    this.classList.add("active")
+  }
 })
