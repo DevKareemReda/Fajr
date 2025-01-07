9bb4217ff6844c9b02b0527cbd33e31507a14176
Kareem Reda <147895396+DevKareemReda@users.noreply.github.com.>
2025-01-06 16:01:43 -0800

    finish website

--- js/classical.js
+++ js/classical.js
@@ -1,8 +1,11 @@
 let allTabs = document.querySelectorAll(".tabs li a");
 allTabs.forEach((el) => {
-    el.onclick = function (e) {
-        e.preventDefault();
-        allTabs.forEach((el) => el.classList.remove("active"));
-        this.classList.add("active");
-    };
+  el.onclick = function (e) {
+    e.preventDefault();
+    allTabs.forEach((el) => el.classList.remove("active"));
+    this.classList.add("active");
+		document.querySelectorAll(".box-tab").forEach(el=> el.classList.remove("active"))
+		document.querySelector("." + this.getAttribute("data-tab")).classList.add("active")
+
+  };
 });
